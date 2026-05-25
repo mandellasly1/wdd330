628a5675d6c0c3436b7844ddb822b8467fb6eaba
mandellasly1 <akpomah_nelson@yahoo.com>
2026-05-25 12:27:24 +0100

    forms

---
+++ startHere/js/scripts.js
@@ -0,0 +1,21 @@
+// const getString = window.location.search;
+// console.log(getString);
+
+const myInfo = new URLSearchParams(getString);
+console.log(myInfo);
+
+// console.log(myInfo.get("first"));
+// console.log(myInfo.get("last"));
+// console.log(myInfo.get("ordinance"));
+// console.log(myInfo.get("date"));
+// console.log(myInfo.get("location"));
+// console.log(myInfo.get("phone"));
+// console.log(myInfo.get("email"));
+
+document.querySelector("#results").innerHTML =
+"<p> Appointment for " + myInfo.get("first") + " " + myInfo.get("last") + "</p>" +
+"<p> Ordinance: " + myInfo.get("ordinance") + "</p>" +
+"<p> Date: " + myInfo.get("date") + "</p>" +
+"<p> Location: " + myInfo.get("location") + "</p>" +
+"<p> Phone: " + myInfo.get("phone") + "</p>" +
+"<p> Email: " + myInfo.get("email") + "</p>";
