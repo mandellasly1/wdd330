f0f03b2d80e60db8bc4d0f7901f397f09c4a1696
mandellasly1 <akpomah_nelson@yahoo.com>
2026-06-18 13:44:36 +0100

    Sleepoutside

---
+++ src/js/ProductData.mjs
@@ -0,0 +1,27 @@
+function convertToJson(res) {
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error("Bad Response");
+  }
+}
+
+export default class ProductData {
+  constructor(category) {
+    this.category = category;
+    this.path = "/json/tents.json${this.category}.json";
+  }
+
+  
+  getData() {
+    return fetch(this.path)
+      .then(convertToJson)
+      .then((data) => data);
+  }
+
+  async findProductById(Id) {
+    const products = await this.getData();
+    // Make sure the property name matches your JSON structure
+    return products.find((item) => item.Id === Id);
+  }
+}
