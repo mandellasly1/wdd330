1b3ff08fd43bb973b5f6a0b7ccddcb8ffdb30fdf
mandellasly1 <akpomah_nelson@yahoo.com>
2026-06-18 17:51:19 +0100

    travel  safe

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,20 +1,25 @@
 import ExternalServices from "./ExternalServices.mjs";
 import ShoppingCart from "./ShoppingCart.mjs";
-import checkoutProcess from "./CheckoutProcess.mjs";   // <-- make sure this file exists
+import checkoutProcess from "./CheckoutProcess";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
 const dataSource = new ExternalServices();
 const checkoutProcess = new CheckoutProcess(dataSource);
-checkoutProcess.displayOrderSummary();
+checkoutProcess.displayOderSummary();
 
-const cart = new ShoppingCart(document.querySelector(".product-list"));
-cart.init();
+const Cart = new ShoppingCart (document.querySelector(".product-list"));
+ShoppingCart.init();
 
 document.getElementById("checkoutForm").addEventListener("submit", evt => {
-  evt.preventDefault();
-  checkoutProcess.checkout(evt.target);
+    evt.preventDefault();
+    checkoutProcess.checkout(evt.target)
+
 });
 
 
+
+/*
+import CheckoutProcess from "./CheckoutProcess.mjs";
+*/
