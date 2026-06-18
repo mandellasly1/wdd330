abbdc5f14592e13e0485e68f08628333350da29d
mandellasly1 <akpomah_nelson@yahoo.com>
2026-06-18 18:02:57 +0100

    Fix checkout.js import and spelling

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,25 +1,20 @@
 import ExternalServices from "./ExternalServices.mjs";
 import ShoppingCart from "./ShoppingCart.mjs";
-import checkoutProcess from "./CheckoutProcess";
+import CheckoutProcess from "./CheckoutProcess.mjs";   // <-- matches your file name
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
 const dataSource = new ExternalServices();
 const checkoutProcess = new CheckoutProcess(dataSource);
-checkoutProcess.displayOderSummary();
+checkoutProcess.displayOrderSummary();   // <-- fixed spelling
 
-const Cart = new ShoppingCart (document.querySelector(".product-list"));
-ShoppingCart.init();
+const cart = new ShoppingCart(document.querySelector(".product-list"));
+cart.init();
 
 document.getElementById("checkoutForm").addEventListener("submit", evt => {
-    evt.preventDefault();
-    checkoutProcess.checkout(evt.target)
-
+  evt.preventDefault();
+  checkoutProcess.checkout(evt.target);
 });
 
 
-
-/*
-import CheckoutProcess from "./CheckoutProcess.mjs";
-*/
